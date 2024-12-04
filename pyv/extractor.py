55baf5a3b7946db05d68228e9d14cdc6b988625b
Kirill Borisov <k.borisov239@gmail.com>
2024-12-04 20:30:53 +0100

    extractor module and tests

---
+++ pyv/extractor.py
@@ -0,0 +1,97 @@
+from pyv.csr import CSRUnit
+from pyv.module import Module
+from pyv.port import Input, Output, Wire, Constant
+from pyv.reg import Reg, Regfile
+from pyv.mem import ReadPort, WritePort
+from pyv.simulator import Simulator
+import pyv.isa as isa
+from pyv.util import getBit, getBits, MASK_32, XLEN, msb_32, signext
+from pyv.log import logger
+from dataclasses import dataclass, field
+
+STOP_INSTR = 0xffffffff
+
+@dataclass
+class IFXT_t:
+    inst: int = 0
+
+@dataclass
+class TXT_t:
+    flush_bits_ready: bool = False
+
+@dataclass
+class XTIF_t:
+    entropy_bits: list = field(default_factory=list)
+    active: bool = False
+    ready: bool = False
+    flush_bits: bool = False
+
+class Extractor(Module):
+    """Entropy extractor.
+
+    Inputs:
+        IFXT_t: Interface from IFStage (instruction)
+        TXT_t:  Interface from top (memory flush ready signal)
+    
+    Outputs:
+        XTIF_t: Interface to IFStage (entropy bits, active, ready)
+    """
+
+    def __init__(self, regf: Regfile, csr: CSRUnit):
+        super().__init__()
+        self.entropy_bits = []     # TODO turn into a register
+        self.regfile = regf
+        self.csr = csr
+        self.ready = False
+        self.ready_out = False
+        self.active_out = True
+        self.flush_bits = False
+
+        # self.registerStableCallbacks([self.check_exception])
+
+        self.IFXT_i = Input(IFXT_t)
+        self.TXT_i = Input(TXT_t)
+        self.XTIF_o = Output(XTIF_t)
+    
+    def process(self):
+        top_val: TXT_t = self.TXT_i.read()
+        flush_ready = top_val.flush_bits_ready
+
+        val: IFXT_t = self.IFXT_i.read()
+        inst = val.inst
+
+        # set ready signal
+        if inst == STOP_INSTR:
+            self.ready = True
+        
+        # set ready output
+        if self.ready and self.active_out:
+            self.ready_out = True
+        
+        # set active output
+        if self.ready and self.active_out:
+            self.active_out = False
+
+        opcode = getBits(inst, 6, 2)
+        funct7 = getBits(inst, 31, 25)
+        # Get entropy bits from funct7
+        if opcode == isa.OPCODES['OP']:
+            self.entropy_bits.append(self.get_entropy_bits(funct7))
+
+        # set flush signal
+        if (self.flush_bits or len(self.entropy_bits) == 16 or (self.ready and len(self.entropy_bits) > 0)) and not flush_ready:
+            self.flush_bits = True
+        else:
+            self.flush_bits = False
+
+        # write output
+        self.XTIF_o.write(XTIF_t(
+            self.entropy_bits, self.active_out, self.ready_out, self.flush_bits
+        ))
+    
+    def get_entropy_bits(self, funct7):
+        bit_high = getBit(funct7, 6)
+        bits_low = getBits(funct7, 4, 0)
+        bits = str(bin(bit_high))[2:] + str(bin(bits_low))[2:]
+        entropy_bits = int(bits, 2)
+        return entropy_bits
---
+++ test/test_extractor.py
@@ -0,0 +1,80 @@
+import pytest
+from pyv.simulator import Simulator
+from pyv.csr import CSRUnit
+from pyv.reg import Regfile
+from pyv.extractor import Extractor, IFXT_t, XTIF_t, TXT_t
+
+@pytest.fixture(scope="function")
+def extractor():
+    regf = Regfile()
+    csr = CSRUnit()
+    extractor = Extractor(regf, csr)
+    extractor._init()
+    return extractor
+
+class TestEntropyExtractor():
+    # initialization and base functionality
+    def test_extractor(self, sim: Simulator, extractor: Extractor):
+        # set input
+        extractor.IFXT_i.write(IFXT_t(0xfaa42633))
+        sim.step()
+
+        # read output and verify
+        out: XTIF_t = extractor.XTIF_o.read()
+        assert out.entropy_bits == [61]
+        assert not out.ready
+        assert out.active
+        assert not out.flush_bits
+    
+    # active/ready functionality
+    def test_extractor_stop(self, sim: Simulator, extractor: Extractor):
+        # set input
+        extractor.IFXT_i.write(IFXT_t(0xffffffff))
+        sim.step()
+
+        # read output and verify
+        out: XTIF_t = extractor.XTIF_o.read()
+        assert out.ready
+        assert not out.active
+    
+    # enrtopy list forming
+    def test_extractor_consecutive(self, sim: Simulator, extractor: Extractor):
+        # set input
+        extractor.IFXT_i.write(IFXT_t(0xfaa42633))  # entropy = 61
+        sim.step()
+
+        extractor.IFXT_i.write(IFXT_t(0xf8a42633))  # entropy = 60
+        sim.step()
+
+        # read output and verify
+        out: XTIF_t = extractor.XTIF_o.read()
+        assert len(out.entropy_bits) == 2
+        assert out.entropy_bits == [61, 60]
+        assert not out.flush_bits
+    
+    # flush signal
+    def test_extractor_flush_signal(self, sim: Simulator, extractor: Extractor):
+        extractor.TXT_i.write(TXT_t(False))
+        # 16 cycles
+        for _ in range(0, 8):
+            # set input 1 and do a cycle
+            extractor.IFXT_i.write(IFXT_t(0xfaa42633))  # entropy = 61
+            sim.step()
+            # set input 2 and do a cycle
+            extractor.IFXT_i.write(IFXT_t(0xf8a42633))  # entropy = 60
+            sim.step()
+
+        # validate output
+        out: XTIF_t = extractor.XTIF_o.read()
+        assert len(out.entropy_bits) == 16
+        assert out.entropy_bits == [61, 60, 61, 60, 61, 60, 61, 60, 61, 60, 61, 60, 61, 60, 61, 60]
+        assert out.flush_bits
+
+        # mimic flush ready signal
+        extractor.TXT_i.write(TXT_t(True))
+        # one more step
+        extractor.IFXT_i.write(IFXT_t(0xfaa42633))  # entropy = 61
+        sim.step()
+        # read output and verify
+        out: XTIF_t = extractor.XTIF_o.read()
+        assert not out.flush_bits
